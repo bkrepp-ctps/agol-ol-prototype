53d531ff73e580567acdb62691d2b96e802c11a1
Ben Krepp <bkrepp@ctps.org>
2021-06-02 14:32:21 -0400

    Added event handler logic for select-town combo box. App currently hits CORS error.

--- prototype.js
+++ prototype.js
@@ -10,12 +10,26 @@ var epsg3857_proj = proj4('EPSG:3857');
 proj4.defs("EPSG:26986", "+proj=lcc +lat_1=42.68333333333333 +lat_2=41.71666666666667 +lat_0=41 +lon_0=-71.5 +x_0=200000 +y_0=750000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs");
 var epsg26986_proj = proj4('EPSG:26986');
 
+// WGS84 SRS
+proj4.defs("EPSG:4326", "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs");
+var wgs_srs = proj4("EPSG:4326");
 
 // URL for AGOL-hosted ESRI "feature service" containing TAZ-level demographic data from last LRTP
+// ==> This one has an SRS of EPSG:26986
 var sample_taz_esri = 'https://services1.arcgis.com/jIRgb54Jq9V3BUeD/ArcGIS/rest/services/sample_taz_demographics/FeatureServer/0';
-
 // For use during development and debugging:
 var wfs_get_caps = 'https://dservices1.arcgis.com/jIRgb54Jq9V3BUeD/arcgis/services/sample_taz_demographics/WFSServer?service=wfs&request=getcapabilities';
+
+// URL for AGOL-hosted ESRI "feature service" containing TAZ-level demographic data from last LRTP
+// ==> This one has an SRS of EPSG:3857
+var sample_taz_esri_3857_wfs = 'https://dservices1.arcgis.com/jIRgb54Jq9V3BUeD/arcgis/rest/services/sample_taz_demographics_3857_wfs_gdb/FeatureServer/0';
+                                              // 'https://services1.arcgis.com/jIRgb54Jq9V3BUeD/arcgis/rest/services/sample_taz_demographics_3857_wfs_gdb/FeatureServer';
+
+// For use during development and debugging:
+var wfs_get_caps_3857 =  'https://dservices1.arcgis.com/jIRgb54Jq9V3BUeD/arcgis/services/sample_taz_demographics_3857_wfs_gdb/WFSServer?service=wfs&request=getcapabilities';
+
+
+
 
 // URLs for MassGIS basemap layer services
 var mgis_serviceUrls = { 
@@ -45,7 +59,7 @@ var vectorDrawingLayer = new ol.layer.Vector({
 function executeTabularQuery(whereClause) {
     arcgisRest
          .queryFeatures({
-             url: sample_taz_esri,
+             url: sample_taz_esri_3857_wfs,    // sample_taz_esri,
             where: whereClause,
             f: "geojson",
             returnGeometry: true,
@@ -70,7 +84,7 @@ function executeTabularQuery(whereClause) {
 function executeSpatialQuery(geometry) {
     arcgisRest
         .queryFeatures({
-              url: sample_taz_esri,
+              url: sample_taz_esri_3857_wfs, //  sample_taz_esri,
               geometry: geometry,
               geometryType: "esriGeometryPolygon",
               spatialRel: "intersects",
@@ -85,6 +99,37 @@ function executeSpatialQuery(geometry) {
 
 // OpenLayers 'map' object:
 var ol_map = null;
+
+var aMpoTowns = [
+    [2, "ACTON"], [10, "ARLINGTON"], [14, "ASHLAND"],
+    [23, "BEDFORD"], [25, "BELLINGHAM"], [26, "BELMONT"], [30, "BEVERLY"], [34, "BOLTON"],
+	[35, "BOSTON"], [37, "BOXBOROUGH"], [40, "BRAINTREE"], [46, "BROOKLINE"], [48, "BURLINGTON"],
+    [49, "CAMBRIDGE"], [50, "CANTON"], [51, "CARLISLE"], [57, "CHELSEA"], [65, "COHASSET"],
+    [67, "CONCORD"],
+    [71, "DANVERS"], [73, "DEDHAM"], [78, "DOVER"], [82, "DUXBURY"],
+    [92, "ESSEX"], [93, "EVERETT"],
+    [99, "FOXBOROUGH"], [100, "FRAMINGHAM"], [101, "FRANKLIN"],
+    [107, "GLOUCESTER"],
+    [119, "HAMILTON"], [122, "HANOVER"], [131, "HINGHAM"], [133, "HOLBROOK"], [136, "HOLLISTON"],
+    [139, "HOPKINTON"], [141, "HUDSON"], [142, "HULL"],
+    [144, "IPSWICH"],
+    [155, "LEXINGTON"], [157, "LINCOLN"], [158, "LITTLETON"], [163, "LYNN"], [164, "LYNNFIELD"],
+    [165, "MALDEN"], [166, "MANCHESTER"], [168, "MARBLEHEAD"], [170, "MARLBOROUGH"], [171, "MARSHFIELD"],
+    [174, "MAYNARD"], [175, "MEDFIELD"], [176, "MEDFORD"], [177, "MEDWAY"], [178, "MELROSE"],
+    [184, "MIDDLETON"], [185, "MILFORD"], [187, "MILLIS"], [189, "MILTON"],
+    [196, "NAHANT"], [198, "NATICK"], [199, "NEEDHAM"], [207, "NEWTON"], [208, "NORFOLK"],
+    [213, "NORTH READING"], [219, "NORWELL"], [220, "NORWOOD"],
+    [229, "PEABODY"], [231, "PEMBROKE"],
+    [243, "QUINCY"],
+    [244, "RANDOLPH"], [246, "READING"], [248, "REVERE"], [251, "ROCKLAND"], [252, "ROCKPORT"],
+    [258, "SALEM"], [262, "SAUGUS"], [264, "SCITUATE"], [266, "SHARON"], [269, "SHERBORN"],
+    [274, "SOMERVILLE"], [277, "SOUTHBOROUGH"], [284, "STONEHAM"], [285, "STOUGHTON"], [286, "STOW"],
+    [288, "SUDBURY"], [291, "SWAMPSCOTT"],
+    [298, "TOPSFIELD"],
+    [305, "WAKEFIELD"], [307, "WALPOLE"], [308, "WALTHAM"], [314, "WATERTOWN"],  [315, "WAYLAND"],
+    [317, "WELLESLEY"], [320, "WENHAM"], [333, "WESTON"], [335, "WESTWOOD"], [336, "WEYMOUTH"],
+    [342, "WILMINGTON"],  [344, "WINCHESTER"], [346, "WINTHROP"], [347, "WOBURN"],  [350, "WRENTHAM"]
+];  // aMpoTowns[];
 
 // Function: initialize()
 //     Initializes OpenLayers map, gets MassGIS basemap service properties by executing AJAX request
@@ -174,6 +219,20 @@ function initialize() {
          // Execute a simple tabular SQL query of the underlying data.
          executeTabularQuery("town='ARLINGTON'");
          
+         // Initialize combo box of towns
+         var i;
+         for (i = 0; i < aMpoTowns.length; i++) {
+             $('#select_town').append($('<option>', { value: aMpoTowns[i][0],     
+                                                                      text : aMpoTowns[i][1] }));  
+            
+        }
+        // Arm on-change event handler for combo box
+        $('#select_town').on('change', function(e) {
+             var town_name = $('#select_town').find(":selected").text();
+             var query_string = "town='" + town_name + "'";
+             executeTabularQuery(query_string);
+        });
+         
          // Beginning of stuff for spatial query driven by sketch
         var draw;
         function addInteraction() {
@@ -184,13 +243,13 @@ function initialize() {
                 var currentFeature= e.feature;
                 var g = currentFeature.getGeometry();
                 // Note: ESRI spatial query is a 'BBOX" query requiring an esriGeometryEnvelope parm: [minx, miny, maxx, maxy]
-                var bbox3857 = g.getExtent();
-                // TBD: (1) Check that the elements of this array are in the correct order
-                //        (2) Reproject to EPSG:26986
-                
+                var bbox = g.getExtent();
                 _DEBUG_HOOK = 1;
+                // Attempts to project from EPSG:3857 to EPSG:26986 using the proj4 library thus far haven't worked.
+                // Instead, we will attempt to query against a copy of the sample TAZ demographics data layer that was projected to EPSG:3857 using ArcMap.
                 // Execute spatial query
-                 executeSpatialQuery(g);              
+                executeSpatialQuery(bbox);              
+                _DEBUG_HOOK = 2;
             });
             ol_map.addInteraction(draw);
         }
